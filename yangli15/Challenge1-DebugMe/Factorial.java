7e1f2f2b35908bfe79abaa546ba6f8d3f64ee19a
seefldt2 <seefldt2@67bb8810-f416-49f7-95ef-536b191976b8>
2013-09-10 15:09:31 +0000

    Push of MP1 on 10 Sep 2013

---
+++ yangli15/Challenge1-DebugMe/Factorial.java
@@ -0,0 +1,20 @@
+//UIUC CS125 FALL 2013 MP. File: Factorial.java, CS125 Project: Challenge1-DebugMe, Version: 2013-09-10T10:09:27-0500.634524579
+
+/**
+ * A program to calculate a factorial. The given code may contain errors. Fix the
+ * given code and add additional code to calculate a factorial and pass the unit
+ * tests. Hint sometimes an 'int' just 'aint big enough.
+ * 
+ * @see Factorial-ReadMe.txt for details on how to complete this program.
+ * @author insert-your-netid-here
+ */
+public class Factorial {
+	public static void main(String[] args) {
+		int max = 0;
+		System.out.println("Enter a number between 1 and 20 inclusive.");
+		while (max < 1 || max >= 21) {
+			max = TextIO.getlnInt();
+		}
+		TextIO.putln(42);
+	}
+}
