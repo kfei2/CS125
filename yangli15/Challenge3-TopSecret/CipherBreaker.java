57267d6356e09281428a7a0f3401519ec0f9805d
seefldt2 <seefldt2@67bb8810-f416-49f7-95ef-536b191976b8>
2013-09-21 17:25:08 +0000

    Push of MP3 on 21 Sep 2013

---
+++ yangli15/Challenge3-TopSecret/CipherBreaker.java
@@ -0,0 +1,15 @@
+//UIUC CS125 FALL 2013 MP. File: CipherBreaker.java, CS125 Project: Challenge3-TopSecret, Version: 2013-09-21T12:24:57-0500.390883635
+/**
+ * See CipherBreaker.txt for instructions.
+ * TODO: add your netid to the line below
+ * 
+ * @author put-your-netid-here
+ */
+public class CipherBreaker {
+
+	public static void main(String[] args) {
+		TextIO.putln("Text?");
+		String line = TextIO.getln();
+		TextIO.putln(line);
+
+}	}
