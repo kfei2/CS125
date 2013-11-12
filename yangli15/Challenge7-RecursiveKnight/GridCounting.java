3c9ecf7df6bfab8c02ad24699448a1464de6968a
seefldt2 <seefldt2@67bb8810-f416-49f7-95ef-536b191976b8>
2013-11-12 17:50:48 +0000

    Push of MP5 on 14 Oct 2013

---
+++ yangli15/Challenge7-RecursiveKnight/GridCounting.java
@@ -0,0 +1,16 @@
+//UIUC CS125 FALL 2013 MP. File: GridCounting.java, CS125 Project: Challenge7-RecursiveKnight, Version: 2013-11-12T11:50:16-0600.415914391
+
+public class GridCounting {
+	/** Returns the total number of possible routes (paths) from
+	 * (x,y) to (tx,ty).
+	 * There are only three valid kinds of moves:
+	 *  Increment x by one.
+	 *  Increment x by two.
+	 *  Increment y by one.
+	 *  
+	 *  Hint: You'll need to test two base cases.
+	 */
+	public static int count(int x,int y, int tx, int ty) {
+		return 57;
+	}
+}
